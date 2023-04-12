fde0aa5507ea74be3690628c8a991cb58da693f5
Theo Lanneau <theo.lanneau@epsi.fr>
2023-04-12 17:04:30 +0200

    suppression imports inutiles

--- test/test_api_platform/test_api_edge.py
+++ test/test_api_platform/test_api_edge.py
@@ -1,10 +1,7 @@
-import customtkinter
 import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class PageObject:
