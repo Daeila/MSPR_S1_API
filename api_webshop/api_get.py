3a706b06694557ac76c086933e48c9dd35cb4015
Theo Lanneau <theo.lanneau@epsi.fr>
2023-07-05 13:25:15 +0200

    correction bug requetes api webshop

--- api_webshop/api_get.py
+++ api_webshop/api_get.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, g
 from api_requests.crm import crm_requests as crm
+from api_requests.erp import erp_requests as erp
 from api_webshop.authenticator import check_authentication
 
 bp = Blueprint("api_get", __name__, url_prefix="/")
@@ -27,3 +28,15 @@ def get_customer_orders(customer_id):
 @check_authentication
 def get_customer_order_details(customer_id, order_id):
     return crm.get_customer_order_details(customer_id, order_id)
+
+
+@bp.route("products", methods=["GET"])
+@check_authentication
+def get_products():
+    return erp.get_products()
+
+
+@bp.route("products/<int:product_id>", methods=["GET"])
+@check_authentication
+def get_product_details(product_id):
+    return erp.get_product_details(product_id)
